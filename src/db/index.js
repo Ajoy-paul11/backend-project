9e8f85dcb2a35b493af0925e500d68e8b6296ac1
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-05-28 22:49:48 +0530

    Handle the async process after DB connection.

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-        console.log(connectionInstance);
+        // console.log(connectionInstance);
     } catch (error) {
         console.log("MongoDB connection ERROR: ", error);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require('dotenv').config({path: './env'}) 
 
+import { app } from './app.js';
 import connectDB from './db/index.js';
 import dotenv from 'dotenv'
 
@@ -8,6 +9,19 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Mongo DB connection failed!! ", err)
+    })
 
 
 
