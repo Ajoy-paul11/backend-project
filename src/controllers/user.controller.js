0bc7280f3d688d95f83dd6611c4ef641ae0ff1ee
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-06-26 01:12:35 +0530

    Typing error resolved in controller `getCurrentUser` at json() response.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -269,7 +269,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
-        .json(200, req.user, "Current User fetched Successfully")
+        .json(
+            new ApiResponse(200, req.user, "Current User fetched Successfully")
+        )
 })
 
 
