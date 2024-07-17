a6e22ef7c4e548970d47089d504a1acf08289275
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-07-17 23:21:45 +0530

    Declare the routes of likes and subscriptions in the app.js file.

--- src/app.js
+++ src/app.js
@@ -21,11 +21,16 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 // Routes declartion
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+
 
 // http://localhost:8000/api/v1/users/register
 
